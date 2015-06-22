a4483fb5415b4e6f2549fb04f1503483d5cf4f51
양정훈 <fog_design@neptunez.co.kr>
2015-06-22 18:50:54 +0900

    move 수정 중

--- Assets/Script/Move.cs
+++ Assets/Script/Move.cs
@@ -28,6 +28,8 @@ public class Move : MonoBehaviour
 
     List<GameObject> enemy;
 
+    GameObject closest;
+
     List<EnemyCollector> enemyCollector;
     GameObject[] enemyCollect;
 
@@ -41,6 +43,7 @@ public class Move : MonoBehaviour
         enemy = new List<GameObject>();
         enemyCollector = new List<EnemyCollector>();
         array = new int[3];
+        closest = null;
 	}
 	
 	// Update is called once per frame
@@ -57,15 +60,20 @@ public class Move : MonoBehaviour
 
             clickPosition = new Vector3((Mathf.Round(clickPosition.x * 100.0f)) / 100.0f, 0, (Mathf.Round(clickPosition.z * 100)) / 100.0f);
 
-            print("click : " + clickPosition.x);
+            //print("click : " + clickPosition.x);
 
             thisPosition = this.transform.position;
             thisPosition.y = 0;
 
-            if (thisPosition != clickPosition)
+            if (thisPosition != clickPosition && closest == null)
             {
                 StopCoroutine("MoveCharac");
                 StartCoroutine("MoveCharac");
+            }
+            else if (closest != null)
+            {
+
+                StartCoroutine("FindMove");
             }
         }
 
@@ -81,7 +89,21 @@ public class Move : MonoBehaviour
         Gizmos.DrawLine(clickPosition, this.transform.position);
     }
 
+    public IEnumerator FindMove()
+    {
+        GameObject findEnemy =  FindEnemy();
 
+        Vector3 runDirection = findEnemy.transform.position - this.transform.position;
+
+        while (runDirection != thisPosition)
+        {
+            transform.Translate((runDirection - thisPosition).normalized * Time.deltaTime * moveSpeed);
+            thisPosition = transform.position;
+
+            yield return null;
+        }
+
+    }
 
     public IEnumerator MoveCharac()
     {
@@ -93,7 +115,6 @@ public class Move : MonoBehaviour
             thisPosition = transform.position;
             thisPosition = new Vector3((Mathf.Round(thisPosition.x * 100.0f)) / 100.0f, 0, (Mathf.Round(thisPosition.z * 100.0f)) / 100.0f );
 
-            //print("speed " + thisPosition);
             yield return null;
         }
 
@@ -103,68 +124,31 @@ public class Move : MonoBehaviour
 
     }
 
-    public void FindEnemy()
+    public GameObject FindEnemy()
     {
 
-        //enemyCollector = GameObject.FindGameObjectsWithTag("Enemy");
-
-        //enemyPosition = new Vector3[enemyCollector.Length];
-
-        ////for (int i = 0; i < enemyCollector.Length; i++)
-        ////    {
-        //        foreach (GameObject ec in enemyCollector)
-        //        {
-        //            for (int i = 0; i < enemyCollector.Length; i++)
-        //            {
-        //                enemyPosition[i] = ec.transform.position;
-        //                print(ec.name);
-        //                print(enemyPosition[i]);    
-        //            }
-                    
-        //        }
-
-        //        print("{0} {1}" + enemyCollector[0].name + enemyPosition[0]);
-        //    //}
-
         enemyCollect = GameObject.FindGameObjectsWithTag("Enemy");
-
-        GameObject test = new GameObject();
-        //Vector3[] vtest = new Vector3();
-
-        //foreach (GameObject item in enemyCollect)
-        //{
-        //    test = item;
-        //    //vtest[] = item.transform.position;
-
-        //    print(item.name + vtest);
-
-        //}
-
-        //for (int i = 0; i < enemyCollect.Length; i++)
-        //{
-        //    vtest[i] = enemyCollect[i].transform.position - this.transform.position;
-        //}
         
+        float distance = Mathf.Infinity;
+        Vector3 position = this.transform.position;
+
+        foreach (GameObject go in enemyCollect)
+        {
+            Vector3 diff = go.transform.position - position;
+            //sqrMagnitude는 
+            float curDistance = diff.sqrMagnitude;
+
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+
+        print(closest.name + closest.transform.position + " / " + distance);
+
+        return closest;
         
-
-        
-
-
-        //foreach (Vector3 item in aPosition)
-        //{
-        //    Vector3 a;
-        //    Vector3 b;
-
-        //    a = item - this.transform.position;
-
-        //}
-
-        //print("HI");
-
-        
-        
-
-
     }
 
     public int this[int index]
@@ -182,26 +166,31 @@ public class Move : MonoBehaviour
         }
     }
 
-    public class EnemyCollector : MonoBehaviour 
+    public void AttackEnemy()
     {
-        private GameObject enemy;
+        playerState = PlayerState.Attack;
+    }
+}
 
-        public GameObject Enemy
-        {
-            get { return enemy; }
-        }
+public class EnemyCollector : MonoBehaviour
+{
+    private GameObject enemy;
 
-        private Vector3 enemyPosition;
+    public GameObject Enemy
+    {
+        get { return enemy; }
+    }
 
-        public Vector3 EnemyPosition
-        {
-            get { return enemyPosition; }
-        }
+    private Vector3 enemyPosition;
 
-        public EnemyCollector(GameObject enemy, Vector3 enemyPosition)
-        {
-            this.enemy = enemy;
-            this.enemyPosition = enemyPosition;
-        }
+    public Vector3 EnemyPosition
+    {
+        get { return enemyPosition; }
+    }
+
+    public EnemyCollector(GameObject enemy, Vector3 enemyPosition)
+    {
+        this.enemy = enemy;
+        this.enemyPosition = enemyPosition;
     }
 }
