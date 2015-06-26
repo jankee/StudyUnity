db30c3a6e0635dc8d2042f24d8f8cec021d9f22d
양정훈 <fog_design@nepturnz.co.kr>
2015-06-26 19:06:48 +0900

    2D Roguelike 4 of 14 파일의 3분대

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour {
+
+    [Serializable]
+    public class Count
+    {
+        public int minimum;
+        public int maximum;
+
+        public Count(int min, int max)
+        {
+            minimum = min;
+            maximum = max;
+        }
+    }
+
+    public int colums = 8;
+    public int rows = 8;
+
+    public Count wallCount = new Count(5, 9);
+    public Count foodCount = new Count(1, 5);
+
+    public GameObject exit;
+
+    public GameObject[] floorTiles;
+    public GameObject[] wallTiles;
+    public GameObject[] foodTiles;
+    public GameObject[] enemyTiles;
+    public GameObject[] outerWallTiles;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
